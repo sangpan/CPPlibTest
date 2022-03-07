42379e2fa3ae93550c950bc68e248873815541f2
sangpan <sangpan@paran.com>
2022-03-07 20:04:49 +0900

    * modify for making windows library

--- B/library1.h
+++ B/library1.h
@@ -4,9 +4,14 @@
 
 #ifndef CPPPLAYGROUND2_LIBRARY1_H
 #define CPPPLAYGROUND2_LIBRARY1_H
+#if defined(_MSC_VER)
+#  define TEST_LIB_DLL_EXPORT __declspec(dllexport)
+#pragma warning(disable : 4251)		//	need to have dll-interface  warn
+#elif defined(__GNUC__)
+#  define TEST_LIB_DLL_EXPORT 
+#endif
 
-
-class library1 {
+class TEST_LIB_DLL_EXPORT library1 {
 
 public:
     static void function000();
